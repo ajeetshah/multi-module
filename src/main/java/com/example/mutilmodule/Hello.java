bdd652c57914e299150732f9aeff1f04e63f9bae
Ajeet <ajeets.shah.87@gmail.com>
2024-12-23 15:55:21 +0530

    Add code using guava

--- src/main/java/com/example/mutilmodule/Hello.java
+++ src/main/java/com/example/mutilmodule/Hello.java
@@ -1,10 +1,16 @@
 package com.example.mutilmodule;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class Hello {
 
-    public static void main() {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
         List<String> names = Lists.newArrayList("John", "Adam", "Jane");
-
         List<String> reversed = Lists.reverse(names);
+        System.out.println(names);
+        System.out.println(reversed);
     }
 }
